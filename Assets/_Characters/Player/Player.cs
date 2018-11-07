622ceacc4995e2f5e73cec73324300fb12dbf1f8
Unknown <lpalicka6@gmail.com>
2018-11-07 21:06:19 +0100

    Project

---
+++ Assets/_Characters/Player/Player.cs
@@ -0,0 +1,153 @@
+﻿﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+// TODO consider re-wire...
+using RPG.CameraUI;
+using RPG.Core;
+using RPG.Weapons;
+
+namespace RPG.Characters
+{
+    public class Player : MonoBehaviour, IDamageable
+    {
+        [SerializeField] float maxHealthPoints = 100f;
+        [SerializeField] float baseDamage = 10f;
+        [SerializeField] Weapon weaponInUse = null;
+        [SerializeField] AnimatorOverrideController animatorOverrideController = null;
+        [SerializeField] AudioClip[] damageSounds;
+        [SerializeField] AudioClip[] deathSounds;
+
+        // Temporarily serialized for dubbing
+        [SerializeField] SpecialAbility[] abilities;
+
+        const string DEATH_TRIGGER = "Death";
+        const string ATTACK_TRIGGER = "Attack";
+
+        AudioSource audioSource;
+        Animator animator;
+        float currentHealthPoints;
+        CameraRaycaster cameraRaycaster;
+        float lastHitTime = 0f;
+
+        public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; } }
+
+        void Start()
+        {
+            RegisterForMouseClick();
+            SetCurrentMaxHealth();
+            PutWeaponInHand();
+            SetupRuntimeAnimator();
+            abilities[0].AttachComponentTo(gameObject);
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void TakeDamage(float damage)
+        {
+			bool playerDies = (currentHealthPoints - damage <= 0); // must ask before reducing health
+            ReduceHealth(damage);          
+            if (playerDies)
+            {
+                StartCoroutine(KillPlayer());
+            }
+        }
+
+        IEnumerator KillPlayer()
+        {
+			animator.SetTrigger(DEATH_TRIGGER);
+
+            audioSource.clip = deathSounds[UnityEngine.Random.Range(0, deathSounds.Length)];
+			audioSource.Play();
+            yield return new WaitForSecondsRealtime(audioSource.clip.length);
+
+            SceneManager.LoadScene(0);
+		}
+
+        private void ReduceHealth(float damage)
+        {
+            currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+			audioSource.clip = damageSounds[UnityEngine.Random.Range(0, damageSounds.Length)];
+			audioSource.Play();
+        }
+
+        private void SetCurrentMaxHealth()
+        {
+            currentHealthPoints = maxHealthPoints;
+        }
+
+        private void SetupRuntimeAnimator()
+        {
+            animator = GetComponent<Animator>();
+            animator.runtimeAnimatorController = animatorOverrideController;
+            animatorOverrideController["DEFAULT ATTACK"] = weaponInUse.GetAttackAnimClip(); // remove const
+        }
+
+        private void PutWeaponInHand()
+        {
+            var weaponPrefab = weaponInUse.GetWeaponPrefab();
+            GameObject dominantHand = RequestDominantHand();
+            var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+            weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+            weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+        }
+
+        private GameObject RequestDominantHand()
+        {
+            var dominantHands = GetComponentsInChildren<DominantHand>();
+            int numberOfDominantHands = dominantHands.Length;
+            Assert.IsFalse(numberOfDominantHands <= 0, "No DominantHand found on Player, please add one");
+            Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHand scripts on Player, please remove one");
+            return dominantHands[0].gameObject;
+        }
+
+        private void RegisterForMouseClick()
+        {
+            cameraRaycaster = FindObjectOfType<CameraUI.CameraRaycaster>();
+            cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
+        }
+
+        void OnMouseOverEnemy(Enemy enemy)
+        {
+            if (Input.GetMouseButton(0) && IsTargetInRange(enemy.gameObject))
+            {
+                AttackTarget(enemy);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                AttemptSpecialAbility(0, enemy);
+            }
+        }
+
+        private void AttemptSpecialAbility(int abilityIndex, Enemy enemy)
+        {
+            var energyComponent = GetComponent<Energy>();
+            var energyCost = abilities[abilityIndex].GetEnergyCost();
+
+            if (energyComponent.IsEnergyAvailable(energyCost))
+            {
+                energyComponent.ConsumeEnergy(energyCost);
+                var abilityParams = new AbilityUseParams(enemy, baseDamage);
+                abilities[abilityIndex].Use(abilityParams);
+            }
+        }
+
+        private void AttackTarget(Enemy enemy)
+        {
+            if (Time.time - lastHitTime > weaponInUse.GetMinTimeBetweenHits())
+            {
+                animator.SetTrigger(ATTACK_TRIGGER);
+                enemy.TakeDamage(baseDamage);
+                lastHitTime = Time.time;
+            }
+        }
+
+        private bool IsTargetInRange(GameObject target)
+        {
+            float distanceToTarget = (target.transform.position - transform.position).magnitude;
+            return distanceToTarget <= weaponInUse.GetMaxAttackRange();
+        }
+    }
+}
---
+++ Assets/_Weapons/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Weapons
+{
+    [CreateAssetMenu(menuName = ("RPG/Weapon"))]
+    public class Weapon : ScriptableObject
+    {
+
+        public Transform gripTransform;
+
+        [SerializeField] GameObject weaponPrefab;
+        [SerializeField] AnimationClip attackAnimation;
+        [SerializeField] float minTimeBetweenHits = .5f;
+        [SerializeField] float maxAttackRange = 2f;
+
+        public float GetMinTimeBetweenHits()
+        {
+            // TODO consdier whether we take animation time into account
+            return minTimeBetweenHits;
+        }
+
+        public float GetMaxAttackRange()
+        {
+            return maxAttackRange;
+        }
+
+        public GameObject GetWeaponPrefab()
+        {
+            return weaponPrefab;
+        }
+        
+        public AnimationClip GetAttackAnimClip()
+        {
+            RemoveAnimationEvents();
+            return attackAnimation;
+        }
+
+        // So that asset packs cannot cause crashes
+        private void RemoveAnimationEvents()
+        {
+            attackAnimation.events = new AnimationEvent[0];
+        }
+    }
+}
